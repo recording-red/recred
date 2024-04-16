87e241bb4da9f6996bdc9099e9202c08a0e1913e
Remi Petitjean <remi.petitjean@gmail.com>
2024-04-16 14:11:54 +0000

    Update push_to_aws.sh

--- push_to_aws.sh
+++ push_to_aws.sh
@@ -2,7 +2,7 @@
 # see https://docs.aws.amazon.com/AmazonECR/latest/userguide/docker-push-ecr-image.html
 
 AWS_REGION=eu-west-3
-AWS_ACCOUNT_ID=465843813700
+AWS_ACCOUNT_ID=123456789
 DOCKER_TAG=recred
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
